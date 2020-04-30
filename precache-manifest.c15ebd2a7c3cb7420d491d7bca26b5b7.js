93d628adc315bcb8a07de2ecc9278d92f523bb1c
irynailina <irynailina@gmail.com>
2020-04-30 20:35:37 +0300

    Updates

---
+++ precache-manifest.c15ebd2a7c3cb7420d491d7bca26b5b7.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "fd17635df3a8c3bbe3b7498649d268d4",
+    "url": "/goit-react-hw-02-feedback/index.html"
+  },
+  {
+    "revision": "f0ff985c9f7c73a134ba",
+    "url": "/goit-react-hw-02-feedback/static/css/main.ae8dc0c1.chunk.css"
+  },
+  {
+    "revision": "9c2368418f4c96579d36",
+    "url": "/goit-react-hw-02-feedback/static/js/2.be1e4b66.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/goit-react-hw-02-feedback/static/js/2.be1e4b66.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "f0ff985c9f7c73a134ba",
+    "url": "/goit-react-hw-02-feedback/static/js/main.7bd07ea5.chunk.js"
+  },
+  {
+    "revision": "921f1dce578c37a88de9",
+    "url": "/goit-react-hw-02-feedback/static/js/runtime-main.912650e5.js"
+  }
+]);
